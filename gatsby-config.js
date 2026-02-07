6b32727d6f4440945c271422f84fdc8942ca4a8b
Ran Xia <xiaranforusa@gmail.com>
2026-02-06 16:49:20 -0800

    Fix CI pipeline

--- gatsby-config.js
+++ gatsby-config.js
@@ -127,7 +127,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/R.jpeg`, // This path is relative to the root of the site.
+        icon: `src/images/R.png`, // This path is relative to the root of the site.
       },
     },
   ],
